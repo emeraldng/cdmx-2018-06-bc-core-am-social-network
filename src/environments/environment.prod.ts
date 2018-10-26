4242330e6e3cee3572737545d20cdbce0159d723
emeraldng <ifglitterthen@gmail.com>
2018-10-25 23:26:23 -0500

    Añadiendo keys de firebase & deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAw3X9i7EnmHc1iw7ZCsRZWaIvfYyRkQv0",
+    authDomain: "burger-queen-ng.firebaseapp.com",
+    databaseURL: "https://burger-queen-ng.firebaseio.com",
+    projectId: "burger-queen-ng",
+    storageBucket: "burger-queen-ng.appspot.com",
+    messagingSenderId: "143465089012"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false, 
+  firebase :{
+    apiKey: "AIzaSyAw3X9i7EnmHc1iw7ZCsRZWaIvfYyRkQv0",
+    authDomain: "burger-queen-ng.firebaseapp.com",
+    databaseURL: "https://burger-queen-ng.firebaseio.com",
+    projectId: "burger-queen-ng",
+    storageBucket: "burger-queen-ng.appspot.com",
+    messagingSenderId: "143465089012"
+  }
 };
 
 /*
