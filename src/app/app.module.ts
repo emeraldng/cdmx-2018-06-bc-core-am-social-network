bc5c4f7ac89c114e005b32ee2b3c5f6280b5560e
emeraldng <ifglitterthen@gmail.com>
2018-10-25 23:51:04 -0500

    Añadiendo componentes login/menus/ordenes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { LoginComponent } from './login/login.component';
+import { DesayunoComponent } from './desayuno/desayuno.component';
+import { ComidaComponent } from './comida/comida.component';
+import { OrdenesComponent } from './ordenes/ordenes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    DesayunoComponent,
+    ComidaComponent,
+    OrdenesComponent
   ],
   imports: [
     BrowserModule,
